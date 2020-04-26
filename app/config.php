0f62e1879cc346565d4a67982056c58daa26a3da
Eduardo Lezama Sanchez <lezamaeduardo079@gmail.com>
2020-04-25 22:14:50 -0500

    primer commit

---
+++ app/config.php
@@ -0,0 +1,10 @@
+<?php
+
+// INFORMACIÓN DE LA BASE DE DATOS EN POSTGRESQL
+
+define('NOMBRE_SERVIDOR', 'localhost');
+define('NOMBRE_USUARIO', 'postgres');
+define('PASSWORD', 'eduardo99');
+define('BASE_DE_DATOS', 'Metalicos');
+
+?>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+include_once("cabecera.html");
+include_once("login.php");
+
+?>
+      
+<?php
+include_once("pie.html");
+?>
